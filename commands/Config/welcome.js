0dda7ea8c39c617482eba6ded760fb6a350890c0
TSLARoadster <51007646+TSLARoadster@users.noreply.github.com>
2021-05-31 10:33:53 -0400

    Fixed mongoDB connection and writing

--- commands/Config/welcome.js
+++ commands/Config/welcome.js
@@ -8,10 +8,13 @@ module.exports = {
   execute: (bot, message, args) => {
     if (!message.member.hasPermission('ADMINISTRATOR')) return;
 
-const role = message.mentions.roles.first() ? message.mentions.roles.first() : message.guild.roles.cache.get(args[0]);
-const channel = message.mentions.channels.first() ? message.mentions.channels.first() : message.guild.channels.cache.get(args[1]);
+    const role = message.mentions.roles.first() ? message.mentions.roles.first() : message.guild.roles.cache.get(args[0]);
+    const channel = message.mentions.channels.first() ? message.mentions.channels.first() : message.guild.channels.cache.get(args[1]);
 
-if(args[0] === undefined || args[1] === undefined) return message.channel.send("Please enter a Valid Role and/or Channel.")
+    if(args[0] === undefined || args[1] === undefined) {
+      message.channel.send("Please enter a Valid Role and/or Channel.");
+      return;
+    }
 
     const MongoClient = require('mongodb').MongoClient;
     const uri = require('../../config.json').uri;
@@ -19,27 +22,26 @@ if(args[0] === undefined || args[1] === undefined) return message.channel.send("
     mclient.connect(async err => {
       if (err) throw err;
       const collection = mclient.db('Sky-Bot').collection('Servers');
+      
       const main = {
         _id: message.guild.id,
         welcomechannel: channel.id,
         welcomerole: role.id,
       };
+      
       await collection.updateOne(
         { _id: message.guild.id },
-        { $set: { welcomechannel: channel.id } },
-        { $set: { welcomerole: role.id } },
+        { $set: { welcomeChannel: channel.id, welcomeRole: role.id } },
         { upsert: true }
       )
-      const foundDocument = await collection.findOne();
-      mclient.close();
 
-
-      const sucembed = new discord.MessageEmbed()
+      const sucEmbed = new discord.MessageEmbed()
         .setTitle('Welcome Setup')
         .setColor('GREEN')
         .setDescription(`Successfully set the WelcomeChannel to ${channel}.\nSuccesfully set the WelcomeRole to ${role}.`)
 
-      message.channel.send(sucembed);
+      await message.channel.send(sucEmbed);
+      mclient.close();
     })
   }
 };
