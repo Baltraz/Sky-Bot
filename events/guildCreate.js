3c783538679dc37b37f4e5d82f6c40eee0dc92ee
TSLARoadster <51007646+TSLARoadster@users.noreply.github.com>
2021-06-05 18:04:13 -0400

    changed 'bot' to 'client'

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -3,27 +3,27 @@ const chalk = require('chalk');
 
 module.exports = {
     name: 'guildCreate',
-    execute(guild, bot) {
+    execute(guild, client) {
         console.log(chalk.yellow(`Joined a Guild: ${guild.name}`));
-        discordLog(bot,
+        discordLog(client,
              new discord.MessageEmbed()
-                .setAuthor(bot.user.username, bot.user.avatarURL())
+                .setAuthor(client.user.username, client.user.avatarURL())
                 .setDescription(`Joined a Guild: \`${guild.name}\``)
                 .setColor('7CFC00')
                 .setTimestamp()
                 .addFields(
                   {name: "Member Count of the Server", value: `${guild.memberCount}`},
-                  {name: "Total Server Count Now", value: `${bot.guilds.cache.size}`},
-                  {name: "Total User Count Now", value: `${bot.users.cache.size}`})
+                  {name: "Total Server Count Now", value: `${client.guilds.cache.size}`},
+                  {name: "Total User Count Now", value: `${client.users.cache.size}`})
         )
     }
 };
 
-function discordLog(bot, embed) {
+function discordLog(client, embed) {
     delete require.cache[require.resolve('../config.json')];
     const config = require('../config.json');
 
-    bot.channels.fetch(config.joinlog)
+    client.channels.fetch(config.joinlog)
         .then(channel => channel.send(embed))
         .catch(console.error)
 }
