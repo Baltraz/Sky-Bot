2a539eead9e357c534fc61ab2436571c1554ace6
Baltraz <>
2021-06-08 19:07:24 +0000

    fixed scammer command

--- commands/WIP/scammer.js
+++ commands/WIP/scammer.js
@@ -1,45 +1,62 @@
-
-const urii = process.env['uri']
 const Discord = require('discord.js');
+const axios = require('axios');
+const sbz = require("../../sbz.json");
+const urii = process.env['uri']
+
 
 module.exports = {
   name: "Scammer",
-  description: "Checks for Scammers in the Database.",
-  usage: "!scammer IGN",
+  description: "Checks the SBZ and Sky Bot Database for Scammers",
+  usage: "!scammer (IGN)",
   perms: "None",
   folder: "Skyblock",
-    execute: (client, message, args) => {
-    message.delete();
+    async execute(client, message, args) {
+
+const ign = args[0];
+
+if(ign === undefined) {
+  message.channel.send('Please enter a Username to check.')
+  return;
+}
+axios.get(`https://some-random-api.ml/mc?username=${ign}`).then(res => {
+				const uuid = res.data.uuid;
+
+
     const MongoClient = require('mongodb').MongoClient;
     const mclient = new MongoClient(urii, { useNewUrlParser: true, useUnifiedTopology: true });
     mclient.connect(async err => {
       if (err) throw err;
       const ign = args[0];
       const collection = mclient.db('Sky-Bot').collection('Scammers');
-      let found = await collection.findOne({ scammerIGN: ign })
-      const innocent = new Discord.MessageEmbed()
-      .setTitle(`${ign} is Innocent.`)
-      .setColor('GREEN')
-      .setDescription('Still be very careful when trading with anybody!')
+      let found = await collection.findOne({ _id: uuid })
 
-      if(ign === undefined) {
-        message.channel.send('Please enter a IGN to check.')
-        return;
-      }
 
-      if (found === null) {
-        message.channel.send(innocent)
-        return;
-      }
 
-      const embed = new Discord.MessageEmbed()
-      .setTitle('SCAMMER!')
-      .setDescription(`**IGN:** ${found.scammerIGN}\n**Reason:** ${found.scamREASON}\n**UUID:** ${found._id}`)
+    if(sbz[uuid]) {
+      const sbzembed = new Discord.MessageEmbed()
+      .setTitle('⚠️USER IS A SCAMMER⚠️')
+      .setDescription(`**IGN:** ${ign}\n**Reason:** ${sbz[uuid]["reason"]}\n**UUID:** ${uuid}`)
       .setColor('RED')
-      
-        message.channel.send(embed)
-
-      mclient.close();
+      message.channel.send(sbzembed)
+      return;
+    } else if (found) {
+      const sbembed = new Discord.MessageEmbed()
+      .setTitle('⚠️USER IS A SCAMMER⚠️')
+      .setDescription(`**IGN:** ${ign}\n**Reason:** ${found.scamREASON}\n**UUID:** ${uuid}`)
+      .setColor('RED')
+      message.channel.send(sbembed)
+      return;
+    } else {
+      const innocent = new Discord.MessageEmbed()
+      .setTitle('<a:yes:847468695772987423> USER IS A INNOCENT')
+      .setDescription(`Still be careful when trading with anyone!`)
+      .setColor('GREEN')
+      message.channel.send(innocent)
     }
-  )}
+      mclient.close();
+
+
+    })
+})
+}
 };
